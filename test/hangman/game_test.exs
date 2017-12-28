60ef16a78c27e7dc152dc18e0cfe221f674a89d7
Kerry Buckley <kerryjbuckley@gmail.com>
2017-12-28 10:22:38 +0000

    Add test

---
+++ test/hangman/game_test.exs
@@ -0,0 +1,13 @@
+defmodule Hangman.GameTest do
+  use ExUnit.Case
+  alias Hangman.Game
+  doctest Game
+
+  describe "Hangman.Game.new_game/0" do
+    test "returns a new game struct" do
+      game = Game.new_game
+      assert game.remaining_guesses == 10
+      assert game.state ==  :initialising
+    end
+  end
+end
