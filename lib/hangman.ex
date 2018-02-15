0a0020b2469c8ea8229914dbd01abd5cabc77719
Kerry Buckley <kerryjbuckley@gmail.com>
2018-02-15 20:38:55 +0000

    Add tally/1 to API

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -2,6 +2,9 @@ defmodule Hangman do
   alias Hangman.Game
 
   defdelegate new_game, to: Game
+
+  defdelegate tally(game), to: Game
+
   def make_move(game, guess) do
     with new_game <- Game.make_move(game, guess) do
       {new_game, Game.tally(new_game)}
